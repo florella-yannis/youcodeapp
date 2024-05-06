1ae0bcf1032fca23397d66290ad597cf42e20b33
florella-yannis <florellayannis@gmail.com>
2024-05-06 22:14:07 +0200

    tanstack
    tanstack query

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -3,16 +3,23 @@
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
-import { Toaster } from 'react-hot-toast';
+import { ToastProvider } from '@/components/ui/toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Toaster />
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <ToastProvider>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </ToastProvider>
+        </SessionProvider>
       </ThemeProvider>
     </>
   );
 };
-
